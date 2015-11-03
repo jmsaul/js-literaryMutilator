64c76c0898504789a360b5ad46ddcd56fea358b5
Jeff Saul <scaleup13@gmail.com>
2015-11-03 11:01:39 -0700

    First attempt at javascript

--- index.php
+++ index.php
@@ -2,12 +2,13 @@
 <html lang=""en>
 	<head>
 		<meta charset="UTF-8" />
+		<title>JavaScript Assignment 1</title>
 	</head>
-	<header>JavaScript Assignment 1</header>
+	<header>bootcamp-coders:  Jeff Saul</header>
 	<body>
 		<h1>JavaScript Assignment 1:  Literary Mutilator</h1>
 		<p>
-			Deflector power at maximum. Energy discharge in six seconds. Warp reactor core primary coolant failure. Fluctuate phaser resonance frequencies. Resistance is futile. Recommend we adjust shield harmonics to the upper EM band when proceeding. These appear to be some kind of power-wave-guide conduits which allow them to work collectively as they perform ship functions. Increase deflector modulation to upper frequency band.
+			Deflector power at maximum. Energy discharge in six seconds. Warp reactor core primary coolant failure. Fluctuate phaser resonance frequencies. <div id="click" onclick="clickEvent();">Resistance is futile</div>. Recommend we adjust shield harmonics to the upper EM band when proceeding. These appear to be some kind of power-wave-guide conduits which allow them to work collectively as they perform ship functions. Increase deflector modulation to upper frequency band.
 		</p>
 		<p>
 			Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. Captain, the most elementary and valuable statement in science, the beginning of wisdom, is ‘I do not know.’ All transporters off.
---
+++ js/javascript-events.js
@@ -0,0 +1,10 @@
+/**
+ * JavaScript Events File
+ **/
+
+ /**
+ * Changes the text on a click event
+ **/
+function clickEvent() {
+	 document.getElementbyId("click").innerHTML = "resistance is good for you"
+ }
