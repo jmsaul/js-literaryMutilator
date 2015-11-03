aca7cc800a333be015244f2a050ce0aa8c26b301
Jeff Saul <scaleup13@gmail.com>
2015-11-03 11:55:14 -0700

    able to replace specified text on click and 1st attempt to change color of 2nd paragraph

--- index.php
+++ index.php
@@ -2,16 +2,19 @@
 <html lang=""en>
 	<head>
 		<meta charset="UTF-8" />
+
+		<!-- Connect to JavaScript File javascript-events.js in js directory -->
+		<script type="text/javascript" src="js/javascript-events.js"></script>
+
 		<title>JavaScript Assignment 1</title>
 	</head>
 	<header>bootcamp-coders:  Jeff Saul</header>
 	<body>
 		<h1>JavaScript Assignment 1:  Literary Mutilator</h1>
 		<p>
-			Deflector power at maximum. Energy discharge in six seconds. Warp reactor core primary coolant failure. Fluctuate phaser resonance frequencies. <div id="click" onclick="clickEvent();">Resistance is futile</div>. Recommend we adjust shield harmonics to the upper EM band when proceeding. These appear to be some kind of power-wave-guide conduits which allow them to work collectively as they perform ship functions. Increase deflector modulation to upper frequency band.
+			Deflector power at maximum. Energy discharge in six seconds. Warp reactor core primary coolant failure. Fluctuate phaser resonance frequencies. <span id="click" onclick="clickEvent();">Resistance is futile</span>. Recommend we adjust shield harmonics to the upper EM band when proceeding. These appear to be some kind of power-wave-guide conduits which allow them to work collectively as they perform ship functions. Increase deflector modulation to upper frequency band.
 		</p>
-		<p>
-			Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. Captain, the most elementary and valuable statement in science, the beginning of wisdom, is ‘I do not know.’ All transporters off.
+		<p id="p2" oneclick="changeFontColor();">Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. Captain, the most elementary and valuable statement in science, the beginning of wisdom, is ‘I do not know.’ All transporters off.
 		</p>
 		<p>
 			Communication is not possible. The shuttle has no power. Using the gravitational pull of a star to slingshot back in time? We are going to Starbase Montgomery for Engineering consultations prompted by minor read-out anomalies. Probes have recorded unusual levels of geological activity in all five planetary systems. Assemble a team. Look at records of the Drema quadrant. Would these scans detect artificial transmissions as well as natural signals?
--- js/javascript-events.js
+++ js/javascript-events.js
@@ -6,5 +6,21 @@
  * Changes the text on a click event
  **/
 function clickEvent() {
-	 document.getElementbyId("click").innerHTML = "resistance is good for you"
+	var changeString="Resistance is good for you";
+  document.getElementById("click").innerHTML = changeString;
  }
+
+/**
+ *  Changes the color of the font
+ **/
+/**  function changefontcolor(theColor,theText)
+ {
+  return '<span style="+theColor+">'+theText+'</span>';
+ }
+**/
+
+function changeFontColor() {
+ var paragraph2 = document.getElementById("p2").innerHTML;
+ paragraph.sytle.color = "blue";
+}
+/** var newString=changeString(changefontcolor("blue",))  **/
