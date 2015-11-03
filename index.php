df92b47dc5c1e0dad01883af8ac402741399c209
Jeff Saul <scaleup13@gmail.com>
2015-11-03 10:24:00 -0700

    upload .gitignore and boilerplate for index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html lang=""en>
+	<head>
+		<meta charset="UTF-8" />
+	</head>
 	<header>JavaScript Assignment 1</header>
 	<body>
 		<h1>JavaScript Assignment 1:  Literary Mutilator</h1>
